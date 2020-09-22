790c85a2120d307689b14e04f470a4823194a799
brobina <bajracharya.robina@gmail.com>
2020-09-22 17:04:39 +0545

    Finally finished

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -3,25 +3,32 @@ import { NavLink } from "react-router-dom";
 import "./Navbar.css";
 
 function Navbar() {
-  return (
-    <div className="navbar">
-      <a className="nav__logo" href="/">
-        <div className="nav__fight"> Fight </div>
-        <div className="nav__covid"> Covid. </div>
-      </a>
-      <ul className="navRight">
-        <li>
-          <NavLink to="/stats"> Data&Statistics </NavLink>
-        </li>
-        <li>
-          <NavLink to="/symptoms"> Symptoms </NavLink>
-        </li>
-        <li>
-          <NavLink to="/prevention"> Prevention </NavLink>
-        </li>
-      </ul>
-    </div>
-  );
+    return ( <
+        div className = "navbar" >
+        <
+        a className = "nav__logo"
+        href = "/" >
+        <
+        div className = "nav__fight" > Fight < /div> <
+        div className = "nav__covid" > Covid. < /div> <
+        /a> <
+        ul className = "navRight" >
+        <
+        li >
+        <
+        NavLink to = "/stats" > Data & Statistics < /NavLink> <
+        /li> <
+        li >
+        <
+        NavLink to = "/symptoms" > Symptoms < /NavLink> <
+        /li> <
+        li >
+        <
+        NavLink to = "/prevention" > Prevention < /NavLink> <
+        /li> <
+        /ul> <
+        /div>
+    );
 }
 
 export default Navbar;
